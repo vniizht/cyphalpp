c41abafd39401e4ec16a5ccad1e72df0bcb01458
Pletenev.Pavel <pletenev.pavel@vniizht.ru>
2022-07-06 17:35:09 +0300

    Add new example

---
+++ navtreedata.js
@@ -0,0 +1,222 @@
+var NAVTREE =
+[
+  [ "cyphalpp", "index.html", [
+    [ "Classes", "annotated.html", [
+      [ "Class List", "annotated.html", [
+        [ "cyphalpp", null, [
+          [ "asio", null, [
+            [ "AsioTimer", "classcyphalpp_1_1asio_1_1AsioTimer.html", [
+              [ "AsioTimer", "classcyphalpp_1_1asio_1_1AsioTimer.html#aced80e2a23531e124c6ae3145818c6de", null ],
+              [ "~AsioTimer", "classcyphalpp_1_1asio_1_1AsioTimer.html#afcfa3e119342442178f306c295e390eb", null ],
+              [ "startSingleShot", "classcyphalpp_1_1asio_1_1AsioTimer.html#af7d9e1f053c617f4f2de84c31de922aa", null ],
+              [ "stop", "classcyphalpp_1_1asio_1_1AsioTimer.html#ad15b2657d365cbe4125760b11bca7751", null ]
+            ] ],
+            [ "AsioUdpSocket", "classcyphalpp_1_1asio_1_1AsioUdpSocket.html", [
+              [ "AsioUdpSocket", "classcyphalpp_1_1asio_1_1AsioUdpSocket.html#a5309c89b8c8bb2834fe629f0b4f34668", null ],
+              [ "~AsioUdpSocket", "classcyphalpp_1_1asio_1_1AsioUdpSocket.html#a869579caaccd2f5b01b4d724314c3364", null ],
+              [ "listenMulticast", "classcyphalpp_1_1asio_1_1AsioUdpSocket.html#a87910e40e76138161d869188e69b67d7", null ],
+              [ "listenUnicast", "classcyphalpp_1_1asio_1_1AsioUdpSocket.html#a87d0b39a8b3c467e51667f16005ce9e0", null ],
+              [ "prepare", "classcyphalpp_1_1asio_1_1AsioUdpSocket.html#a7db22bc4eb7c8af5a0a424c194c0de2a", null ],
+              [ "writeTo", "classcyphalpp_1_1asio_1_1AsioUdpSocket.html#adf87d2962bfa54ba71e3e78891529ae5", null ]
+            ] ]
+          ] ],
+          [ "qt", null, [
+            [ "Enum", "structcyphalpp_1_1qt_1_1Enum.html", [
+              [ "Value", "structcyphalpp_1_1qt_1_1Enum_1_1Value.html", [
+                [ "description", "structcyphalpp_1_1qt_1_1Enum_1_1Value.html#a50fc72246539f972ae4f949b9125bc4a", null ],
+                [ "name", "structcyphalpp_1_1qt_1_1Enum_1_1Value.html#a4d716a758e00c159d9bc0b39d361c52f", null ],
+                [ "value", "structcyphalpp_1_1qt_1_1Enum_1_1Value.html#aa3392e010afd288ba0301b80744b3eb0", null ]
+              ] ],
+              [ "Enum", "structcyphalpp_1_1qt_1_1Enum.html#a300e674120a78bddfc16bfe0eadffd0e", null ],
+              [ "getDescription", "structcyphalpp_1_1qt_1_1Enum.html#a69046d334083da7c10afcb9b654e1bd6", null ],
+              [ "getName", "structcyphalpp_1_1qt_1_1Enum.html#a529c5c6d32b5563fab4e5e83d5c8f0a2", null ],
+              [ "getValue", "structcyphalpp_1_1qt_1_1Enum.html#a4e64517266a025929720924100073410", null ],
+              [ "descriptions", "structcyphalpp_1_1qt_1_1Enum.html#a8bd33bc5ddf01f2118260d925f6b782b", null ],
+              [ "names", "structcyphalpp_1_1qt_1_1Enum.html#ae0d2d7d6c312563bc377c48077a78a32", null ],
+              [ "values", "structcyphalpp_1_1qt_1_1Enum.html#a0f647a25a72744412ec525661700ab58", null ]
+            ] ],
+            [ "FileServer", "classcyphalpp_1_1qt_1_1FileServer.html", [
+              [ "FileServer", "classcyphalpp_1_1qt_1_1FileServer.html#a8ba5e108b50795dc9d79691c1ebc1ebe", null ],
+              [ "~FileServer", "classcyphalpp_1_1qt_1_1FileServer.html#ab1533afdf9cf3d01375f52c48985e0dd", null ],
+              [ "folder", "classcyphalpp_1_1qt_1_1FileServer.html#afd33a79d0c88b6ff679ab6fc08c3a0fc", null ]
+            ] ],
+            [ "NodeData", "structcyphalpp_1_1qt_1_1NodeData.html", [
+              [ "dataFetched", "structcyphalpp_1_1qt_1_1NodeData.html#a3e3d32c6a35a6250870d146c7eb5833d", null ],
+              [ "dataFetching", "structcyphalpp_1_1qt_1_1NodeData.html#ac213e1dfbef3415082ca1dd27a704160", null ],
+              [ "heartbeat", "structcyphalpp_1_1qt_1_1NodeData.html#ae9803bd476a4ecdfa92617de114d75e8", null ],
+              [ "info", "structcyphalpp_1_1qt_1_1NodeData.html#a3f14d255ed6ef99041df78536948d882", null ],
+              [ "timeout", "structcyphalpp_1_1qt_1_1NodeData.html#a98e1cdac62c36534170794ba3c9ec662", null ]
+            ] ],
+            [ "NodeDataField", "structcyphalpp_1_1qt_1_1NodeDataField.html", [
+              [ "Field", "structcyphalpp_1_1qt_1_1NodeDataField.html#a914c2aa70cb1974862ee364b6835f461", [
+                [ "Heartbeat", "structcyphalpp_1_1qt_1_1NodeDataField.html#a914c2aa70cb1974862ee364b6835f461a83622997d22f5f18b9b09bd364cb673e", null ],
+                [ "Info", "structcyphalpp_1_1qt_1_1NodeDataField.html#a914c2aa70cb1974862ee364b6835f461a4059b0251f66a18cb56f544728796875", null ]
+              ] ],
+              [ "description", "structcyphalpp_1_1qt_1_1NodeDataField.html#a20cb1331ff8f066fc3e107f98745df69", null ],
+              [ "field", "structcyphalpp_1_1qt_1_1NodeDataField.html#a9f41239446a39306f9f2671e5da0d3ee", null ],
+              [ "get", "structcyphalpp_1_1qt_1_1NodeDataField.html#a3b1cca33c8f256090e2c74cea198369a", null ],
+              [ "getEdit", "structcyphalpp_1_1qt_1_1NodeDataField.html#a4a8fa3f81c412ff7a7a3a1ff34e8e525", null ],
+              [ "getTooltip", "structcyphalpp_1_1qt_1_1NodeDataField.html#afd74d3f3bf2d071b7584647970de0c13", null ],
+              [ "name", "structcyphalpp_1_1qt_1_1NodeDataField.html#a7087de14b897144f4a415c1702418248", null ]
+            ] ],
+            [ "NodeRegisters", "classcyphalpp_1_1qt_1_1NodeRegisters.html", [
+              [ "NodeRegisters", "classcyphalpp_1_1qt_1_1NodeRegisters.html#a62120be8a07f3eddd7206da24e57df52", null ],
+              [ "~NodeRegisters", "classcyphalpp_1_1qt_1_1NodeRegisters.html#a0a32b678a0b510f9f02547b3b7d0261e", null ],
+              [ "columnCount", "classcyphalpp_1_1qt_1_1NodeRegisters.html#ae17bfaf319bd0550996593fbca921317", null ],
+              [ "data", "classcyphalpp_1_1qt_1_1NodeRegisters.html#a237812ca76272bdb0ef8ee3d80d25315", null ],
+              [ "flags", "classcyphalpp_1_1qt_1_1NodeRegisters.html#adb84a42854278acb42ecf88025de9ecd", null ],
+              [ "headerData", "classcyphalpp_1_1qt_1_1NodeRegisters.html#aff34bf9817847c65d0099474aad064c9", null ],
+              [ "rowCount", "classcyphalpp_1_1qt_1_1NodeRegisters.html#aa81b12f26907ee7b39004072f22f783e", null ],
+              [ "setData", "classcyphalpp_1_1qt_1_1NodeRegisters.html#a5656d7871f62cb034c5e606767aea64d", null ]
+            ] ],
+            [ "NodesHealthModel", "classcyphalpp_1_1qt_1_1NodesHealthModel.html", [
+              [ "NodesHealthModel", "classcyphalpp_1_1qt_1_1NodesHealthModel.html#a39850fab4dd3713ea6ea305c90c4a465", null ],
+              [ "~NodesHealthModel", "classcyphalpp_1_1qt_1_1NodesHealthModel.html#a0fd1265ddfd09280a609200332758c8e", null ],
+              [ "columnCount", "classcyphalpp_1_1qt_1_1NodesHealthModel.html#a63ac90be61574cf31967a82142277de0", null ],
+              [ "data", "classcyphalpp_1_1qt_1_1NodesHealthModel.html#a33fbd9f94e746b3d222e379d240581da", null ],
+              [ "headerData", "classcyphalpp_1_1qt_1_1NodesHealthModel.html#a7577e2234bb968659d3f2395cba6a9fe", null ],
+              [ "rowCount", "classcyphalpp_1_1qt_1_1NodesHealthModel.html#a4b9118a6cbb8195de190938c1e5d5697", null ]
+            ] ],
+            [ "QCyphalTimer", "classcyphalpp_1_1qt_1_1QCyphalTimer.html", [
+              [ "QCyphalTimer", "classcyphalpp_1_1qt_1_1QCyphalTimer.html#ad1690e5519c4baccf2962d4434b9b0a7", null ],
+              [ "~QCyphalTimer", "classcyphalpp_1_1qt_1_1QCyphalTimer.html#a620bc6d189b5a684d056c1245513d7b3", null ],
+              [ "startSingleShot", "classcyphalpp_1_1qt_1_1QCyphalTimer.html#ab9293940f0a8088679a631560d4c25aa", null ],
+              [ "stop", "classcyphalpp_1_1qt_1_1QCyphalTimer.html#ab858f91316c81b3326580b6d131ef747", null ]
+            ] ],
+            [ "QCyphalUdpSocket", "classcyphalpp_1_1qt_1_1QCyphalUdpSocket.html", [
+              [ "QCyphalUdpSocket", "classcyphalpp_1_1qt_1_1QCyphalUdpSocket.html#a61c71409e7621c98deb0adabe4397f9f", null ],
+              [ "~QCyphalUdpSocket", "classcyphalpp_1_1qt_1_1QCyphalUdpSocket.html#ac11181ed45c1b3e2cc359f5c30bcf0b6", null ],
+              [ "listenMulticast", "classcyphalpp_1_1qt_1_1QCyphalUdpSocket.html#a650e7a5b824481add8dee2a8d1c9b56c", null ],
+              [ "listenUnicast", "classcyphalpp_1_1qt_1_1QCyphalUdpSocket.html#a1312a435d4ff40315913a16ef9b38e8a", null ],
+              [ "prepare", "classcyphalpp_1_1qt_1_1QCyphalUdpSocket.html#ad9e3e09582c947779b3b387219791f3d", null ],
+              [ "writeTo", "classcyphalpp_1_1qt_1_1QCyphalUdpSocket.html#a192716c62089a871db6dcb27ede8b427", null ]
+            ] ]
+          ] ],
+          [ "CyphalUdp", "structcyphalpp_1_1CyphalUdp.html", [
+            [ "ServiceCaller", "classcyphalpp_1_1CyphalUdp_1_1ServiceCaller.html", [
+              [ "call", "classcyphalpp_1_1CyphalUdp_1_1ServiceCaller.html#aaeab2defba939264d2e250db26ac95bf", null ],
+              [ "operator ()", "classcyphalpp_1_1CyphalUdp_1_1ServiceCaller.html#a762d7cc5db05b6756b0b2091cf39fe22", null ],
+              [ "CyphalUdp", "classcyphalpp_1_1CyphalUdp_1_1ServiceCaller.html#a91fc3c5ce138d064724d456de280356f", null ]
+            ] ],
+            [ "CyphalUdp", "structcyphalpp_1_1CyphalUdp.html#a03ca31ca25a5581abc1d326e081f2598", null ],
+            [ "prepareServiceCalls", "structcyphalpp_1_1CyphalUdp.html#a9272db93e3d560bd0973018fdea2bed2", null ],
+            [ "sendMessage", "structcyphalpp_1_1CyphalUdp.html#aa46d76062a07c6bcfc6e2760e839c3dc", null ],
+            [ "sendMessage", "structcyphalpp_1_1CyphalUdp.html#a99b89d35c9289eccdd359cf0f3cb0eb2", null ],
+            [ "setAddr", "structcyphalpp_1_1CyphalUdp.html#ae0103791c5f76a0b1c70dd999d77b714", null ],
+            [ "subscribeMessage", "structcyphalpp_1_1CyphalUdp.html#a177d59bd43dc5a5078372c4836c9a1ea", null ],
+            [ "subscribeMessage", "structcyphalpp_1_1CyphalUdp.html#a1b76cc9b1cee9f1052132d38c3c969d5", null ],
+            [ "subscribeServiceRequest", "structcyphalpp_1_1CyphalUdp.html#a9f640d2ed1c2295a6d3dad41f064be72", null ],
+            [ "subscribeServiceRequest", "structcyphalpp_1_1CyphalUdp.html#acc62932ef34c6dd234a307704a80aaef", null ]
+          ] ],
+          [ "DataSpecifier", "structcyphalpp_1_1DataSpecifier.html", [
+            [ "Role", "structcyphalpp_1_1DataSpecifier.html#a73be019c9ca769543b548e5a9fab4448", [
+              [ "Message", "structcyphalpp_1_1DataSpecifier.html#a73be019c9ca769543b548e5a9fab4448a5726b00ac273b79b88d27ae01d3be4b0", null ],
+              [ "ServiceRequest", "structcyphalpp_1_1DataSpecifier.html#a73be019c9ca769543b548e5a9fab4448a6164fd4363cf6e2e5da25251015c4f35", null ],
+              [ "ServiceResponce", "structcyphalpp_1_1DataSpecifier.html#a73be019c9ca769543b548e5a9fab4448a73d526972ce4da9499d59ccba7437060", null ]
+            ] ],
+            [ "operator==", "structcyphalpp_1_1DataSpecifier.html#ab4b2e114685e5a48c0775e93f6a71275", null ],
+            [ "node_id", "structcyphalpp_1_1DataSpecifier.html#a4789bd14e21c04944a2f19e1f299f342", null ],
+            [ "role", "structcyphalpp_1_1DataSpecifier.html#a665367d24d54b8cdd8c72c5a5fe4a61a", null ],
+            [ "subject_id", "structcyphalpp_1_1DataSpecifier.html#a85ed3b70be4b04a354dd38a393489505", null ]
+          ] ],
+          [ "HeartbeatPublish", "classcyphalpp_1_1HeartbeatPublish.html", [
+            [ "HeartbeatPublish", "classcyphalpp_1_1HeartbeatPublish.html#aa1867ebcaa80514770a9539daa93566f", null ],
+            [ "heart", "classcyphalpp_1_1HeartbeatPublish.html#a65120affdb87ab3f5938fe59e3ef5c53", null ]
+          ] ],
+          [ "MessageAddr", "structcyphalpp_1_1MessageAddr.html", [
+            [ "MessageAddr", "structcyphalpp_1_1MessageAddr.html#af612db60dee865917b1ee655f4c04c37", null ],
+            [ "MessageAddr", "structcyphalpp_1_1MessageAddr.html#afe620d8284c4436b5c4c9138e7b50142", null ],
+            [ "MessageAddr", "structcyphalpp_1_1MessageAddr.html#a2cf1638d87729163d67130057a4b8b8b", null ],
+            [ "isValidMessage", "structcyphalpp_1_1MessageAddr.html#a52a0c4e08ab70356016ca025e5152342", null ],
+            [ "operator uint32_t", "structcyphalpp_1_1MessageAddr.html#a42e50a2d6174fb9d4acae61ad5571f7d", null ],
+            [ "prefix_9bit", "structcyphalpp_1_1MessageAddr.html#abdae333c681279ae12623a2dc3786095", null ],
+            [ "subject_or_node_id", "structcyphalpp_1_1MessageAddr.html#a919f4bfd6237da50351d7dac27d40047", null ],
+            [ "subnet_id", "structcyphalpp_1_1MessageAddr.html#a607454b0c0b2b62a1c87a36efc190e80", null ]
+          ] ],
+          [ "NetworkAddress", "structcyphalpp_1_1NetworkAddress.html", [
+            [ "addr", "structcyphalpp_1_1NetworkAddress.html#a27c62869bbf06ea808549eac1aaab1d1", null ],
+            [ "port", "structcyphalpp_1_1NetworkAddress.html#abda28b6006e5e0aa67c28473e28f4d90", null ]
+          ] ],
+          [ "TimerImpl", "structcyphalpp_1_1TimerImpl.html", [
+            [ "callback_t", "structcyphalpp_1_1TimerImpl.html#a1f55c365d16cc7f1f0520c8c9e9f5af1", null ],
+            [ "~TimerImpl", "structcyphalpp_1_1TimerImpl.html#a73e2ee804eb680a907e227b18c61afa8", null ],
+            [ "startSingleShot", "structcyphalpp_1_1TimerImpl.html#a0977dcdc047ad7d6c67e617045357278", null ],
+            [ "stop", "structcyphalpp_1_1TimerImpl.html#a5977c6c7fbf2962fb839fad00ce2db32", null ]
+          ] ],
+          [ "TransferMetadata", "structcyphalpp_1_1TransferMetadata.html", [
+            [ "data", "structcyphalpp_1_1TransferMetadata.html#ab10980beaa5b3610db263d4cc496ea16", null ],
+            [ "priority", "structcyphalpp_1_1TransferMetadata.html#a19e1cace646e6525db2b24e64a2a583f", null ],
+            [ "transfer_id", "structcyphalpp_1_1TransferMetadata.html#ae45ba4d21b24fcd3613196e93665c457", null ]
+          ] ],
+          [ "UdpSocketImpl", "structcyphalpp_1_1UdpSocketImpl.html", [
+            [ "~UdpSocketImpl", "structcyphalpp_1_1UdpSocketImpl.html#ac2a1a953116ce87ed0fd933816b367be", null ],
+            [ "listenMulticast", "structcyphalpp_1_1UdpSocketImpl.html#af5dbe59f6d01441ca6694048ad04b9a5", null ],
+            [ "listenUnicast", "structcyphalpp_1_1UdpSocketImpl.html#ab2cd78f13bf14bda6954ede84bf285cd", null ],
+            [ "prepare", "structcyphalpp_1_1UdpSocketImpl.html#ae9f5e5fb415e58a1f6a9173dd27bd340", null ],
+            [ "writeTo", "structcyphalpp_1_1UdpSocketImpl.html#a5bb29f3268e73bc705b16641b7fdc46f", null ]
+          ] ]
+        ] ]
+      ] ],
+      [ "Class Index", "classes.html", null ],
+      [ "Class Hierarchy", "hierarchy.html", [
+        [ "cyphalpp::CyphalUdp", "structcyphalpp_1_1CyphalUdp.html", null ],
+        [ "cyphalpp::CyphalUdp::ServiceCaller< Service >", "classcyphalpp_1_1CyphalUdp_1_1ServiceCaller.html", null ],
+        [ "cyphalpp::DataSpecifier", "structcyphalpp_1_1DataSpecifier.html", null ],
+        [ "cyphalpp::HeartbeatPublish< clock_t >", "classcyphalpp_1_1HeartbeatPublish.html", null ],
+        [ "cyphalpp::MessageAddr", "structcyphalpp_1_1MessageAddr.html", null ],
+        [ "cyphalpp::NetworkAddress", "structcyphalpp_1_1NetworkAddress.html", null ],
+        [ "cyphalpp::qt::Enum< E, T, field >", "structcyphalpp_1_1qt_1_1Enum.html", null ],
+        [ "cyphalpp::qt::Enum< E, T, field >::Value", "structcyphalpp_1_1qt_1_1Enum_1_1Value.html", null ],
+        [ "cyphalpp::qt::NodeData", "structcyphalpp_1_1qt_1_1NodeData.html", null ],
+        [ "cyphalpp::qt::NodeDataField", "structcyphalpp_1_1qt_1_1NodeDataField.html", null ],
+        [ "cyphalpp::TimerImpl", "structcyphalpp_1_1TimerImpl.html", [
+          [ "cyphalpp::asio::AsioTimer", "classcyphalpp_1_1asio_1_1AsioTimer.html", null ],
+          [ "cyphalpp::qt::QCyphalTimer", "classcyphalpp_1_1qt_1_1QCyphalTimer.html", null ]
+        ] ],
+        [ "cyphalpp::TransferMetadata", "structcyphalpp_1_1TransferMetadata.html", null ],
+        [ "cyphalpp::UdpSocketImpl", "structcyphalpp_1_1UdpSocketImpl.html", [
+          [ "cyphalpp::asio::AsioUdpSocket", "classcyphalpp_1_1asio_1_1AsioUdpSocket.html", null ],
+          [ "cyphalpp::qt::QCyphalUdpSocket", "classcyphalpp_1_1qt_1_1QCyphalUdpSocket.html", null ]
+        ] ],
+        [ "QAbstractTableModel", null, [
+          [ "cyphalpp::qt::NodeRegisters", "classcyphalpp_1_1qt_1_1NodeRegisters.html", null ],
+          [ "cyphalpp::qt::NodesHealthModel", "classcyphalpp_1_1qt_1_1NodesHealthModel.html", null ]
+        ] ],
+        [ "QObject", null, [
+          [ "cyphalpp::qt::FileServer", "classcyphalpp_1_1qt_1_1FileServer.html", null ]
+        ] ],
+        [ "QTimer", null, [
+          [ "cyphalpp::qt::QCyphalTimer", "classcyphalpp_1_1qt_1_1QCyphalTimer.html", null ]
+        ] ],
+        [ "QUdpSocket", null, [
+          [ "cyphalpp::qt::QCyphalUdpSocket", "classcyphalpp_1_1qt_1_1QCyphalUdpSocket.html", null ]
+        ] ]
+      ] ],
+      [ "Class Members", "functions_all.html", [
+        [ "All", "functions_all.html", null ],
+        [ "Functions", "functions_func.html", null ]
+      ] ]
+    ] ],
+    [ "Files", "files.html", [
+      [ "File List", "files.html", [
+        [ "asio", "dir_4ba9d796fb6c1382b4d23df2585b80de.html", null ],
+        [ "include", "dir_d44c64559bbebec7f509842c48db8b23.html", null ],
+        [ "qt", "dir_0f63d6441a2c250919e1e8723011eb2f.html", [
+          [ "qt_dsdl_interface", "dir_178c909798df20684d9f2784f05ff8c5.html", null ]
+        ] ]
+      ] ]
+    ] ],
+    [ "Examples", "examples.html", [
+      [ "basic_qt_publisher/basic_qt_publisher.pro", "basic_qt_publisher_2basic_qt_publisher_8pro-example.html", null ],
+      [ "basic_qt_publisher/main.cpp", "basic_qt_publisher_2main_8cpp-example.html", null ]
+    ] ]
+  ] ]
+];
+
+var NAVTREEINDEX =
+[
+".html"
+];
+
+var SYNCONMSG = 'click to disable panel synchronization';
+var SYNCOFFMSG = 'click to enable panel synchronization';
